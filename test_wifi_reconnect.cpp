f624573e2bf2a9677ba2b99a84c6967bff482a5f
Aaron Settles <aaron_settles@cable.comcast.com>
2016-01-20 23:59:44 -0700

    Adding some retry logic based on some new functions added into the ESP8266 library

--- test_wifi_reconnect.cpp
+++ test_wifi_reconnect.cpp
@@ -21,46 +21,123 @@
 #include <ESP8266.h>
 #include <SoftwareSerial.h>
 #include <RGBTools.h>
+#include "DHT.h"
 
 #define SSID        ""
 #define PASSWORD    ""
-#define HOST_NAME   "10.0.0.8"
+#define HOST_NAME   ""
 #define HOST_PORT   (8090)
 #define URI_STRING_BUFFER_LENGTH   (64)
+#define HTTP_RETRIES (3)
+#define HTTP_REQ_TIMEOUT (10000)
+#define HTTP_DELAY_BETWEEN_RETRY (5000);
+
+#define DHTPIN 4     // what digital pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 #define DBG_RX_PIN 13
 #define DBG_TX_PIN 12
 
-#define TEMP_ID     "567c133d7625422fc7a9f4e8"
-int REDPin = 9;    // RED pin of the LED to PWM pin 2
-int GREENPin = 10;  // GREEN pin of the LED to PWM pin 3
-int BLUEPin = 11;   // BLUE pin of the LED to PWM pin 4
+#define TEMP_ID     "t1"
+#define HUMID_ID    "h1"
 
-RGBTools rgb(REDPin,GREENPin,BLUEPin, COMMON_CATHODE);
+#define UPDATE_DELAY 300000
+//#define UPDATE_DELAY 20000
+
+int REDPin = 9;    // RED pin of the LED to PWM pin 9
+int GREENPin = 11;  // GREEN pin of the LED to PWM pin 11
+int BLUEPin = 10;   // BLUE pin of the LED to PWM pin 10
 
 ESP8266 wifi = ESP8266();
+DHT dht(DHTPIN, DHTTYPE);
+RGBTools rgb(REDPin, GREENPin, BLUEPin, COMMON_CATHODE);
 
 SoftwareSerial debugSerial2 = SoftwareSerial(DBG_RX_PIN, DBG_TX_PIN);
 
 bool wifiConnected = false;
 
-void connectToWifi() {
+/* RGB LED Methods*/
+void setRed() {
+	rgb.setColor(255, 0, 0);
+}
 
+void setGreen() {
+	rgb.setColor(0, 255, 0);
+}
 
+void setBlue() {
+	rgb.setColor(0, 0, 255);
+}
 
-	if (wifi.joinAP(SSID, PASSWORD)) {
-		wifiConnected = true;
-		debugSerial2.print(F("Join AP success\r\n"));
-		rgb.setColor(0,255,0);
-	} else {
-		wifiConnected = false;
-		debugSerial2.print(F("Join AP failure\r\n"));
-		rgb.setColor(255,0,0);
+void setOrange() {
+	rgb.setColor(255, 136, 0);
+}
+
+void setWhite() {
+	rgb.setColor(255, 255, 255);
+}
+
+void setBlack() {
+	rgb.setColor(0, 0, 0);
+}
+
+void setTeal() {
+	rgb.setColor(0, 242, 255);
+}
+
+void setPurple() {
+	rgb.setColor(140, 0, 255);
+}
+
+void setYellow() {
+	rgb.setColor(200, 200, 0);
+}
+
+/*RGB Animations*/
+void blinkGreen(int times) {
+	for (int i = 0; i < times; i++) {
+		setGreen();
+		delay(250);
+		setBlack();
+		delay(250);
 	}
 }
 
+/*ESP8266 Methods*/
+void connectToWifi() {
+	setTeal();
+	if (wifi.joinAP(SSID, PASSWORD)) {
+		wifiConnected = true;
+		debugSerial2.print(F("Join AP success\r\n"));
+		blinkGreen(3);
+		setGreen();
+	} else {
+		wifiConnected = false;
+		debugSerial2.print(F("Join AP failure\r\n"));
+		setRed();
+	}
+}
+
+String checkAndReleaseConnection() {
+	debugSerial2.print(F("Current Status: "));
+	String status = wifi.getIPStatus();
+	debugSerial2.println(status);
+	if (status.indexOf("STATUS:3") != -1) {
+		if (wifi.releaseTCP()) {
+			debugSerial2.print(F("release tcp ok\r\n"));
+		} else {
+			debugSerial2.print(F("release tcp err\r\n"));
+		}
+	} else {
+		debugSerial2.println(F("no connection to release"));
+	}
+	return status;
+}
+
 void save_value(String sensorId, String value) {
+	value.replace(" ", "");
 	//Build a HTTP GET string to store the update
+//	String URI_STRING = "GET /ping/";
 	String URI_STRING = "GET /u/";
 	URI_STRING += wifi.getApMac();
 	URI_STRING += "/";
@@ -68,6 +145,13 @@ void save_value(String sensorId, String value) {
 	URI_STRING += "?rv=";
 	URI_STRING += value;
 	URI_STRING += " HTTP/1.0\r\n\r\n";
+//	String URI_STRING = "GET /api/postvalue/?token=";
+//	URI_STRING+=API_KEY;
+//	URI_STRING += "&variable=";
+//	URI_STRING += sensorId;
+//	URI_STRING += "&value=";
+//	URI_STRING += value;
+//	URI_STRING += "\r\n\r\n";
 	debugSerial2.print(F("URI_STRING.length(): "));
 	debugSerial2.println(URI_STRING.length());
 	debugSerial2.print(F("URI_STRING: "));
@@ -76,68 +160,104 @@ void save_value(String sensorId, String value) {
 	memset(uriCharBuffer, 0, URI_STRING_BUFFER_LENGTH);
 	URI_STRING.toCharArray(uriCharBuffer, URI_STRING_BUFFER_LENGTH);
 
-	uint8_t responseBuffer[256] = { 0 };
-
-	if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
-		debugSerial2.print(F("create tcp ok\r\n"));
-	} else {
-		debugSerial2.print(F("create tcp err\r\n"));
-		wifiConnected = false;
-	}
-	if (wifiConnected) {
-		wifi.send((const uint8_t*) uriCharBuffer, strlen(uriCharBuffer));
-
-		uint32_t len = wifi.recv(responseBuffer, sizeof(responseBuffer), 10000);
-		debugSerial2.print(F("Current Status: "));
-		String status = wifi.getIPStatus();
-		debugSerial2.println(status);
-		if (len > 0) {
-			debugSerial2.print(F("Received:["));
-			for (uint32_t i = 0; i < len; i++) {
-				debugSerial2.print((char) responseBuffer[i]);
-			}
-			debugSerial2.print(F("]\r\n"));
+//	uint8_t responseBuffer[256] = { 0 };
+	for (int i = 0; i < HTTP_RETRIES; i++) {
+		bool tcpEstablished = false;
+		if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
+			tcpEstablished = true;
+			debugSerial2.print(F("create tcp ok\r\n"));
 		} else {
-			wifiConnected = false;
+			debugSerial2.print(F("create tcp err\r\n"));
+			setYellow();
+			if ( i + 1 == HTTP_RETRIES) {
+				wifiConnected = false;
+				setRed();
+			}
 		}
+		if (wifiConnected && tcpEstablished) {
+			bool httpGetOk = false;
+			if (!httpGetOk) {
+				wifi.send((const uint8_t*) uriCharBuffer,
+						strlen(uriCharBuffer));
 
-		if (status.indexOf("STATUS:3") != -1) {
-			if (wifi.releaseTCP()) {
-				debugSerial2.print(F("release tcp ok\r\n"));
+				//		uint32_t len = wifi.recv(responseBuffer, sizeof(responseBuffer), 10000);
+
+				httpGetOk = wifi.recvHTTP( HTTP_REQ_TIMEOUT);
+				debugSerial2.print(F("httpGetOk: "));
+				debugSerial2.println(httpGetOk);
+			}
+
+//		if (len > 0) {
+//			debugSerial2.print(F("Received:["));
+//			for (uint32_t i = 0; i < len; i++) {
+//				debugSerial2.print((char) responseBuffer[i]);
+//			}
+//			debugSerial2.print(F("]\r\n"));
+//		} else {
+//			wifiConnected = false;
+//		}
+
+			if (httpGetOk) {
+				setGreen();
+				checkAndReleaseConnection();
+				break;
 			} else {
-				debugSerial2.print(F("release tcp err\r\n"));
+				setYellow();
+				checkAndReleaseConnection();
 			}
+
+			int delayTime = HTTP_DELAY_BETWEEN_RETRY
+			;
+			debugSerial2.print(F("Sleeping between retry: "));
+			debugSerial2.println(delayTime * i + 1);
+			delay(delayTime * i + 1);
 		} else {
-			debugSerial2.println(F("no connection to release"));
+			debugSerial2.println(F("Failed TCP connection test"));
+			setYellow();
 		}
-	} else {
-		debugSerial2.println(
-				F("WiFi not connected, failed TCP connection test"));
 	}
+
+}
+
+/*Arduino Methods*/
+void doRGBPOST() {
+	for (int i = 0; i < 5; i++) {
+		setWhite();
+		delay(500);
+		setBlack();
+		delay(500);
+	}
+
+	setRed();
+	delay(1000);
+	setGreen();
+	delay(1000);
+	setBlue();
+	delay(1000);
+	setWhite();
 }
 
 void setup(void) {
-	  pinMode(REDPin, OUTPUT);
-	  pinMode(GREENPin, OUTPUT);
-	  pinMode(BLUEPin, OUTPUT);
-//	  for (int i = 0; i < 255; i++) {
-	  	  	rgb.setColor(255,0,0);
-	  		delay(1000);
-	  		rgb.setColor(0,255,0);
-	  		delay(1000);
-	  		rgb.setColor(0,0,255);
-	  		delay(1000);
-//	  	}
+	pinMode(REDPin, OUTPUT);
+	pinMode(GREENPin, OUTPUT);
+	pinMode(BLUEPin, OUTPUT);
+	doRGBPOST();
 	debugSerial2.begin(115200);
 	debugSerial2.print(F("setup begin\r\n"));
+	setPurple();
 	wifi.begin();
-	debugSerial2.print(F("Kicking ESP result: "));
-	debugSerial2.println(wifi.kick());
+	if (wifi.kick()) {
+		blinkGreen(1);
+	} else {
+		setOrange();
+	}
+
 	debugSerial2.print(F("FW Version:"));
 	debugSerial2.println(wifi.getVersion().c_str());
 
 	if (wifi.setOprToStationSoftAP()) {
 		debugSerial2.print(F("to station + softap ok\r\n"));
+		blinkGreen(2);
 	} else {
 		debugSerial2.print(F("to station + softap err\r\n"));
 	}
@@ -160,11 +280,24 @@ void loop(void) {
 	}
 
 	if (wifiConnected) {
-		save_value("testId", "testVal");
+		float f = dht.readTemperature(true);
+		float h = dht.readHumidity();
+
+		debugSerial2.print(F("Temp: "));
+		debugSerial2.print(f);
+		debugSerial2.print(F(" Humid: "));
+		debugSerial2.println(h);
+		save_value(HUMID_ID, String(h));
+		delay(5000);
+		save_value(TEMP_ID, String(f));
+//		save_value("testId", "testVal");
 	} else {
+		rgb.setColor(255, 0, 0);
 		debugSerial2.println(
 				F("WiFi not connected, not attempting TCP connection"));
 	}
-	delay(5000);
+	wifi.printFreeMem();
+
+	delay(UPDATE_DELAY);
 }
 
